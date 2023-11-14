9de30905ad01b4502272158bdf9a5267057ae1a6
ThainaMedeiros06 <thaina.santos4@estudante.ifms.edu.br>
2023-11-14 10:10:20 -0400

    14/11

--- src/aplicacao/listar-filme.use-case.ts
+++ src/aplicacao/listar-filme.use-case.ts
@@ -1,13 +1,11 @@
 import FilmeRepositorioInterface from "./filme-repositorio-interface"
 
 export default class ListarFilme{
-
-    constructor(private bancoInterface:FilmeRepositorioInterface){}
+    constructor(private filmeRepositorio:FilmeRepositorioInterface){}
     public async execute():Promise<Filme[]>{
         return this.filmeRepositorio.listar()
     }
 }
-
 type Filme = {
     id:number,
     titulo:string,
